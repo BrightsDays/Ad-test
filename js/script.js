bc7865c1bd4daaeca140a7c0c7dad5c672d6bc60
Viacheslav <brightsdayss@gmail.com>
2020-03-23 09:37:17 +0300

    style/js for IE

--- js/script.js
+++ js/script.js
@@ -1,26 +1,15 @@
-window.addEventListener("DOMContentLoaded", () => {
-    "use strict";
+const productsOrder = document.querySelector('.products__order'),
+      orderButton = productsOrder.querySelector('button'),
+      productsList = document.querySelectorAll('.products__card');
 
-    const productsOrder = document.querySelector('.products__order'),
-          productsList = document.querySelectorAll('.products__card');
-
-    const changeButton = () => {
-        console.log(productsList.length)
-        if (productsList.length % 2 === 0) {
-            console.log('lo')
-            productsOrder.style.margin = '0 auto';
-            if (window.width > 600 || document.body.clientWidth > 600) {
-                productsOrder.style.gridColumnStart = '1';
-                productsOrder.style.gridColumnEnd = '3';
-            } else {
-                productsOrder.style.gridColumnStart = '1';
-                productsOrder.style.gridColumnEnd = '1';
-            }
-        }
+function changeButton() {
+    if (productsList.length % 2 === 0) {
+        orderButton.style.margin = '0 auto';
+        productsOrder.style.maxWidth = '100%';
     }
+}
 
-    changeButton();
-    window.addEventListener(`resize`, () => {
+changeButton();
+window.addEventListener('resize', function() {
         changeButton();
-    });
 });
